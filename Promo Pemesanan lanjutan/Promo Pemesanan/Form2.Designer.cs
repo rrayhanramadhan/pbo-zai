e26ab85732981e65567cdbaca3e197efd145a953
rayhan ramadhan <117635384+rrayhanramadhan@users.noreply.github.com>
2023-06-21 23:51:12 +0700

    Add files via upload

---
+++ Promo Pemesanan lanjutan/Promo Pemesanan/Form2.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace Promo_Pemesanan
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            textBox1 = new TextBox();
+            button1 = new Button();
+            label2 = new Label();
+            label3 = new Label();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(180, 127);
+            label1.Name = "label1";
+            label1.Size = new Size(142, 20);
+            label1.TabIndex = 0;
+            label1.Text = "Tiket yang Dipesan :";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(334, 124);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(180, 27);
+            textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(334, 171);
+            button1.Name = "button1";
+            button1.Size = new Size(180, 29);
+            button1.TabIndex = 2;
+            button1.Text = "Gunakan Promo";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(151, 231);
+            label2.Name = "label2";
+            label2.Size = new Size(171, 20);
+            label2.TabIndex = 3;
+            label2.Text = "Promo Yang Digunakan :";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(334, 231);
+            label3.Name = "label3";
+            label3.Size = new Size(50, 20);
+            label3.TabIndex = 4;
+            label3.Text = "label3";
+            // 
+            // Form2
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(button1);
+            Controls.Add(textBox1);
+            Controls.Add(label1);
+            Name = "Form2";
+            Text = "pemesanan";
+            Load += Form2_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private TextBox textBox1;
+        private Button button1;
+        private Label label2;
+        private Label label3;
+    }
+}
---
+++ Promo Pemesanan lanjutan/Promo Pemesanan/Form2.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Promo_Pemesanan;
+
+namespace Promo_Pemesanan
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            label3.Text = promodigunakan;
+        }
+        public string promodigunakan { get; set; }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (int.TryParse(textBox1.Text, out int textBoxValue))
+            {
+                Form1 form1 = new Form1();
+                form1.label16value = textBoxValue; // Mengatur nilai properti LabelValue di Form 2
+                form1.ShowDialog(); // Menampilkan Form2 sebagai dialog
+                this.Hide(); // Menyembunyikan Form1
+            }
+        }
+    }
+}
